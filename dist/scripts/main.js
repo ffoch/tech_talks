2e2543ef2501593ebac9ac58c3acb07c5ab2f2c8
fefoch <fefoch@hotmail.com>
2025-02-02 15:08:22 -0300

    criando

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -596,7 +596,26 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"3cYfC":[function(require,module,exports,__globalThis) {
-console.log("ola mundo");
+AOS.init();
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const MinutoEmMs = 60000;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / MinutoEmMs);
+    const seguntosAteOEvento = Math.floor(distanciaAteOEvento % MinutoEmMs / 1000);
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${seguntosAteOEvento}s`;
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Expirado';
+    }
+}, 1000);
 
 },{}]},["k51Nd","3cYfC"], "3cYfC", "parcelRequire94c2")
 
